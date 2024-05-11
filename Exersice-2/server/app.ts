bd24fe4b815fc5a851c88b32f6144aebf470ab55
LahiruHarshana <lharshana2002@gmail.com>
2024-05-11 15:10:14 +0530

    updated

--- Exersice-2/server/app.ts
+++ Exersice-2/server/app.ts
@@ -1,5 +1,22 @@
-function greet(person: string) {
-    return "Hello, " + person;
+// function greet(person: string) {
+//     return "Hello, " + person;
+// }
+//
+// console.log(greet("TypeScript"));
+
+interface Animal {
+    run(): void;
 }
 
-console.log(greet("TypeScript"));
+
+class Human implements Animal {
+    run() {
+        console.log("Human is running");
+    }
+
+}
+class Dog implements Animal {
+    run() {
+        console.log("Dog is running");
+    }
+}
