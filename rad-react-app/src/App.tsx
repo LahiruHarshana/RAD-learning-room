669935779dd75503bec58078df129ba4d0cddcf9
LahiruHarshana <lharshana2002@gmail.com>
2024-05-25 09:49:27 +0530

    updated

--- rad-react-app/src/App.tsx
+++ rad-react-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Home } from './components/home/home';
-
+import {NavBar} from './view/navBar/NavBar';
+ 
 function App() {
   return (
       <>
+      <NavBar/>
         <Home />
       </>
   );
---
+++ rad-react-app/src/view/navBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class NavBar extends Component {
+  render() {
+    return (
+      <div className='p-2 bg-blue-300'>
+        
+        <h1 className='text-1xl text-center'>
+            this is Navbar
+        </h1>
+      </div>
+    )
+  }
+}
