9465c47fe761e3b5a6286532e3686e076609c6ef
LahiruHarshana <lharshana2002@gmail.com>
2024-05-25 10:55:53 +0530

    updated

--- rad-react-app/src/view/mainContent/MainContent.tsx
+++ rad-react-app/src/view/mainContent/MainContent.tsx
@@ -3,17 +3,17 @@ import React, { Component } from 'react';
 export default class MainContent extends Component {
   render() {
     return (
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen flex flex-col bg-amber-100">
         {/* Header */}
         
 
         {/* Main Content Area */}
-        <div className="flex flex-grow">
+        <div className="flex flex-grow bg-amber-100">
           {/* Sidebar */}
           
 
           {/* Main Content */}
-          <main className="flex-grow p-6 bg-gray-100">
+          <main className="flex-grow p-6  bg-amber-100" >
             <h1 className="text-2xl font-bold mb-4">Main Content</h1>
             {/* Add your main content here */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
